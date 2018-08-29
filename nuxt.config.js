a616a1642adf70ef44aab1bbcb15be16faf342d9
Roman Turov <razorfromrds@mail.ru>
2018-08-29 07:03:28 +0700

    Start Build Up

---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-bootstrap',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt, Vue-Bootstrap, Storybook' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  css: [
+    'assets/scss/styles.scss',
+    'bootstrap-vue/dist/bootstrap-vue.css'
+  ],
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  modules: [
+    // 'bootstrap-vue/nuxt'
+    // Or if you have custom bootstrap CSS...
+    ['bootstrap-vue/nuxt', { css: false }]
+  ]
+}
+
---
+++ pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <div>
+      <b-button>I am a Button</b-button>
+      <b-button href="#">I am a Link</b-button>
+    </div>
+    <b-dropdown 
+      id="ddown1" 
+      text="Dropdown Button" 
+      class="m-md-2">
+      <b-dropdown-item>First Action</b-dropdown-item>
+      <b-dropdown-item>Second Action</b-dropdown-item>
+      <b-dropdown-item>Third Action</b-dropdown-item>
+      <b-dropdown-divider/>
+      <b-dropdown-item>Something else here...</b-dropdown-item>
+      <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+    </b-dropdown>
+  </section>
+</template>
+
+<script>
+
+</script>
